07676704f8019c7bf67cbd4b5db300953ac9d7fb
lmachegger <lmachegger@gmail.com>
2021-01-10 14:46:24 +0100

    updated styles, open social links in new tab

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,8 +4,8 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <a href="https://www.instagram.com/macheggerlukas/">Instagram</a>
-    <a href="https://www.youtube.com/channel/UCHCKuTU_wdbhN94YTtYTJTA">Youtube</a>
+    <a target="_blank" href="https://www.instagram.com/macheggerlukas/">Instagram</a>
+    <a target="_blank" href="https://www.youtube.com/channel/UCHCKuTU_wdbhN94YTtYTJTA">Youtube</a>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
