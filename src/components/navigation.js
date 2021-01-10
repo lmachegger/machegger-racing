4345af89f79e7962a2284486e15a22c19304b5ad
lmachegger <lmachegger@gmail.com>
2021-01-10 14:25:08 +0100

    changed style, added simple contact

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,8 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
+    <a href="https://www.instagram.com/macheggerlukas/">Instagram</a>
+    <a href="https://www.youtube.com/channel/UCHCKuTU_wdbhN94YTtYTJTA">Youtube</a>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
